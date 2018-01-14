88ada25fa78812f4da04140f3ba5cac852af8319
Erik Henriksson <erikhenriksson91@gmail.com>
2018-01-14 21:02:59 +0100

    Added Board class with constructor functions
    Board class with constructor initial constructor functions added. Currently
    only a function to generate a list of Box objects, representing a row on
    the Board. Fixed minor bugs in car_game.h.
    
    Added some dummy functionality to Board constructors.
    
    Signed-off-by: Erik Henriksson <erikhenriksson91@gmail.com>

--- src/car_game.cpp
+++ src/car_game.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <list>
 #include "car_game.h"
 
 using namespace std;
@@ -10,4 +8,27 @@ Box::Box(){
 Box::Box(bool occu){
     occupied=occu;
 };
+
+Board::Board(){
+    list<int> l1;
+    l1.push_back(1);
+};
+Board::Board(int row, int col){
+   list<int> l1;
+   l1.push_back(row);
+};
+
+list<Box> Board::generate_row(int col){
+    list<Box> l;
+    for(int i = 1; i <= col; i++){
+        l.push_back(Box());
+    }
+    return l;
+};
+
+//Board::populate_board(int row, int col){
+//    list<Box> row = [Box()];
+//    list<list<Box>> board = [row];
+//    return board;
+//}
 
--- src/car_game.h
+++ src/car_game.h
@@ -2,6 +2,9 @@
 // Includes definitions of class:
 //      Box
 //--------------------------------
+#include <list>
+
+using namespace std;
 
 class Box{
         bool occupied;
@@ -10,4 +13,15 @@ class Box{
         Box(bool);
         void set_occupancy(bool b) {occupied = b;}
         int is_occupied(void) {return occupied;}
+};
+
+class Board{
+// Board descibes the gameboard, private class varaible
+// is a list of lists, where the lists represents rows. The lists
+// contains objects Boxes, one per column.
+    public:
+        Board(void);
+        Board(int, int);
+    private:
+        list<Box> generate_row(int);
 };
